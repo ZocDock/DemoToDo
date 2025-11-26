4132c1c911c9263e3f0b55535e0672659e0f5c79
zocdock <49199150+ZocDock@users.noreply.github.com>
2025-11-26 17:31:01 +0100

    added cross icon

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
 function addTask(){
-    if(inputBox.value === ''){
+    if(inputBox.value === ''){ // if empty
         alert("Please enter a task");
     } else {
         let li = document.createElement('li');
         li.innerText = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = '';
 }
